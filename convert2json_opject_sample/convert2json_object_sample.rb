faa4e77b050762af041f4a63aa12f3cac2f4c4e4
ahirugumi <puts.helloworld@nifty.com>
2013-01-29 00:38:17 +0900

    ERBの出力結果の変更とERBの使い方を変更

--- convert2json_opject_sample/convert2json_object_sample.rb
+++ convert2json_opject_sample/convert2json_object_sample.rb
@@ -1,20 +1,21 @@
 require 'csv'
 require 'erb'
 
-csv = CSV.open("ファイル名.csv", 'r')
+csv = CSV.open("hoge.csv", 'r')
 csv.shift
-e = ERB.new <<EOS, nil, '-'
-<% csv.each do |i| -%>
-{
-    "address":"<%= i[0] %>",
-    "host":"<%= i[1] %>",
-    "enviroment":"<%= i[2] %>",
-    "use":"<%= i[3] %>",
-    "instance":"<%= i[4] %>",
-    "admin":"<%= i[5] %>",
-    "user":"<%= i[6] %>",
-    "url":"<%= i[7] %>"
-}
-<% end -%>
-EOS
-e.run
+ERB.new(DATA.read, nil, '-').run(binding)
+__END__
+var servers = {
+    "web_servers": [<% csv.each_with_index do |i,j| -%>
+        <%= ","unless j==0 -%>{
+            "address":"<%= i[0] %>",
+            "host": "<%= i[1] %>",
+            "enviroment": "<%= i[2] %>",
+            "use": "<%= i[3] %>",
+            "instance": "<%= i[4] %>",
+            "admin": "<%= i[5] %>",
+            "user": "<%= i[6] %>",
+            "url": "<%= i[7] %>"
+        }<% end -%>
+    ]
+};
